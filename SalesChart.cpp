fe5b30167b7758b65df9b86dfa9de636a5e72ab6
teetreats <alexathompson01@icloud.com>
2026-09-05 20:53:50 -0500

    Remove comments and fix formatting

--- SalesChart.cpp
+++ SalesChart.cpp
@@ -1,24 +1,23 @@
-//Alexa Thompson
 #include <iostream>
 using namespace std;
 int main()
 
 {
-    const int num_Of_Stores = 6, //number of stores
-        min_Number = 0,          //starting value
-        fifthy = 50;             //the value for sales
+    const int num_Of_Stores = 6, 
+        min_Number = 0,          
+        fifthy = 50;            
 
-    int store_1, store_2,        //the amount for the stores
+    int store_1, store_2,        
         store_3, store_4,
         store_5, store_6,
 
-        store_1_50s, store_2_50s,           //calculations for the stores
+        store_1_50s, store_2_50s,         
         store_3_50s, store_4_50s,
         store_5_50s, store_6_50s; 
 
-    cout << "Enter today's sales for store 1: ";  //get sales input from the user
+    cout << "Enter today's sales for store 1: "; 
     cin >> store_1;
-    store_1_50s = store_1 / fifthy;               //represent the calcuations number of asterisks for each store
+    store_1_50s = store_1 / fifthy;               
 
     cout << "Enter today's sales for store 2: ";
     cin >> store_2;
@@ -41,11 +40,11 @@ int main()
     store_6_50s = store_6 / fifthy;
 
     cout << "\nSALES BAR CHART" << endl;
-    cout << "(Each * = $50)" << endl;              //each astrisk is $50 dollars
+    cout << "(Each * = $50)" << endl;          
 
     cout << "Store 1: ";
-    for (int x = min_Number; x < store_1_50s; x++)        //initalization expression of the for Loop
-        cout << '*';                                      //and post-fix increment operator
+    for (int x = min_Number; x < store_1_50s; x++)        
+        cout << '*';                                     
     cout << endl;
 
     cout << "Store 2: ";
@@ -73,5 +72,5 @@ int main()
         cout << '*';
     cout << endl;
 
-    return 0;                                        //end command
+    return 0;                                      
 }
